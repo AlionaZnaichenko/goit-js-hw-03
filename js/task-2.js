f31d1abdc8afdde1ba8d33dc5942eb53fdceb2bb
Aliona Znaichenko <ualenaya@gmail.com>
2026-01-19 22:18:29 +0200

    hw

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,6 @@
 function makeArray(firstArray, secondArray, maxLength) {
-  return firstArray.concat(secondArray).slice(0, maxLength);
+  const allArray = firstArray.concat(secondArray);
+  return allArray.slice(0, maxLength);
 }
 console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
